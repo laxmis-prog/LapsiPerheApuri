b3727b7e535023d4bc6ab816ee2ebbfd155e4501
laxmis-prog <laxmisanuu5@gmail.com>
2024-10-29 10:16:06 +0200

    migrations

--- config.py
+++ config.py
@@ -10,12 +10,11 @@ class Config:
         ['true', 'on', '1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    print("MAIL_USERNAME: "+MAIL_USERNAME)
+    SOVELLUSMALLI_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    SOVELLUSMALLI_MAIL_SENDER = 'Sovellusmalli Admin <flasky@example.com>'
+    SOVELLUSMALLI_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql+mysqlconnector://root:""@localhost/flasky'
 
     @staticmethod
     def init_app(app):
@@ -27,6 +26,15 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
+class XamppConfig(Config):
+    DEBUG = True
+    DB_USERNAME= os.environ.get('DB_USERNAME') or 'root'
+    DB_PASSWORD= os.environ.get('DB_PASSWORD') or ''
+    DB_HOST= os.environ.get('DB_HOST') or 'localhost'
+    DB_PORT= os.environ.get('DB_PORT') or '3306'
+    DB_NAME= os.environ.get('DB_NAME') or 'flasky'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://'+DB_USERNAME+':'+DB_PASSWORD+'@'+DB_HOST+':'+DB_PORT+'/'+DB_NAME
+    print("SQLALCHEMY_DATABASE_URI: "+SQLALCHEMY_DATABASE_URI)
 
 class TestingConfig(Config):
     TESTING = True
@@ -43,6 +51,7 @@ config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
+    'xampp': XamppConfig,
 
     'default': DevelopmentConfig
 }
--- flasky.py
+++ flasky.py
@@ -1,16 +1,23 @@
 import os
-from dotenv import load_dotenv
 import click
 from flask_migrate import Migrate
 from app import create_app, db
 from app.models import User, Role, Permission
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-# Load environment variables from .env file
-load_dotenv()
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost:3306/flasky"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
-
+# db.create_all()
 
 @app.shell_context_processor
 def make_shell_context():
@@ -20,10 +27,10 @@ def make_shell_context():
 @app.cli.command()
 @click.argument('test_names', nargs=-1)
 def test(test_names):
-   """Run the unit tests."""
-   import unittest
-   if test_names:
-       tests = unittest.TestLoader().loadTestsFromNames(test_names)
-   else:
-       tests = unittest.TestLoader().discover('tests')
-   unittest.TextTestRunner(verbosity=2).run(tests)
+    """Run the unit tests."""
+    import unittest
+    if test_names:
+        tests = unittest.TestLoader().loadTestsFromNames(test_names)
+    else:
+        tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
